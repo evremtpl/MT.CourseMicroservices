f5bd73d170dea38a86bc89545c41c7ec935c08f2
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-25 21:15:55 +0300

    *[CourseController ve Controller altyapısı kodlanmıştır.]

---
+++ Shared/MT.FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MT.FreeCourse.Shared.Dtos;
+
+namespace MT.FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode=response.StatusCode
+            };
+        }
+    }
+}
