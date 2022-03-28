c8d80882777cfca6d8ce83c93ef8aec4aa5f145e
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-28 21:47:28 +0300

    *[Identity server a IdentityResourse(claims) configurasyonları eklenmiştir.]

--- IdentityServer/MT.FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/MT.FreeCourse.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace MT.FreeCourse.IdentityServer
@@ -20,7 +21,10 @@ namespace MT.FreeCourse.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-
+                       new IdentityResources.Email(),
+                        new IdentityResources.OpenId(),
+                        new IdentityResources.Profile(),
+                       new IdentityResource(){ Name="roles", DisplayName="Roles", Description="User Roles" , UserClaims= new []{ "role"} }
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -45,7 +49,26 @@ namespace MT.FreeCourse.IdentityServer
                    AllowedScopes={ "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
                 },
 
+                    new Client
+                {
+                   ClientName="Asp.Net Core MVC",
+                   ClientId="webMvcClientForUser",
+                   ClientSecrets = { new  Secret("secret".Sha256()) },
+                   AllowedGrantTypes={ new string (GrantType.ResourceOwnerPassword) },
+                   AllowedScopes=
+                        {
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "roles" //kullanıcı online olmasa bile refresh tokenla access token alınabilir
+                        },
+                   AccessTokenLifetime=1*60*60,
+                   RefreshTokenExpiration=TokenExpiration.Absolute,
+                   AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                   RefreshTokenUsage=TokenUsage.ReUse
 
+                },
             };
     }
 }
