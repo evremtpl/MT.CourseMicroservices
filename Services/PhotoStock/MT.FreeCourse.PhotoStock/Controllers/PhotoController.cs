37e295ee6f061d215e4e964cd1a6f7c9f719d422
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-29 10:18:06 +0300

    *[ PhotoStock service --> PhotoController/PhotoDelete]

--- Services/PhotoStock/MT.FreeCourse.PhotoStock/Controllers/PhotoController.cs
+++ Services/PhotoStock/MT.FreeCourse.PhotoStock/Controllers/PhotoController.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using MT.FreeCourse.PhotoStock.Dtos;
 using MT.FreeCourse.Shared.ControllerBases;
 using MT.FreeCourse.Shared.Dtos;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,22 +15,35 @@ namespace MT.FreeCourse.PhotoStock.Controllers
     {
 
         [HttpPost]
-        public async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken)
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
         {
-            if(photo!=null && photo.Length>0)
+            if (photo != null && photo.Length > 0)
             {
-                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photo.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
 
                 using var stream = new FileStream(path, FileMode.Create);
-                    await photo.CopyToAsync(stream, cancellationToken);
-                
-                var returnPath="photos/"+ photo.FileName;
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = "photos/" + photo.FileName;
 
 
                 PhotoDto photoDto = new() { Url = returnPath }; //C# 9
-                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
             }
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found",404));
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
         }
     }
 }
