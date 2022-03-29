fa8bd662a167c9dab49112a9e33cadd959599748
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-29 22:18:20 +0300

    *[Discount service interface oluşturuldu.]

---
+++ Services/Discount/MT.FreeCourse.Discount/Model/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MT.FreeCourse.Discount.Model
+{
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreateTime { get; set; }
+
+    }
+}
---
+++ Services/Discount/MT.FreeCourse.Discount/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,21 @@
+﻿using MT.FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MT.FreeCourse.Discount.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Model.Discount>>> GetAll();
+
+        Task<Response<Model.Discount>> GetById(int id);
+
+        Task<Response<NoContent>> Create(Model.Discount discount);
+
+        Task<Response<NoContent>> Update(Model.Discount discount);
+
+        Task<Response<NoContent>> Delete(int id);
+
+        Task<Response<Model.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
