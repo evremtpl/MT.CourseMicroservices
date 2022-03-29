5520733e65e07b1de8d7e76be3deb9fa67a659b4
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-29 11:44:00 +0300

    *[Basket API BasketService oluşturuldu.}

---
+++ Services/Basket/MT.FreeCourse.Basket/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using MT.FreeCourse.Basket.Dtos;
+using MT.FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT.FreeCourse.Basket.Services.Interfaces
+{
+    interface IBasketService
+    {
+        Task <Response<BasketDto>> GetBasket (string userId);
+        Task<Response<bool>> CreateOrUpdate(BasketDto basketDto);
+       public Task<Response<bool>> Delete(string userId);
+    }
+}
