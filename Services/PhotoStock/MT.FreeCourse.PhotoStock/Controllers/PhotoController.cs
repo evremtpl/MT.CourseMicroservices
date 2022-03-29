35b61ccf2b0f799a5ee33ebe5ec770e9fbd3ba73
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-29 10:09:01 +0300

    *[PhotoStock projesinin oluşturulması ] *[PhotoController/PhotoSave endpoint]

---
+++ Services/PhotoStock/MT.FreeCourse.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MT.FreeCourse.PhotoStock.Dtos;
+using MT.FreeCourse.Shared.ControllerBases;
+using MT.FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MT.FreeCourse.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken)
+        {
+            if(photo!=null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                    await photo.CopyToAsync(stream, cancellationToken);
+                
+                var returnPath="photos/"+ photo.FileName;
+
+
+                PhotoDto photoDto = new() { Url = returnPath }; //C# 9
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+        }
+    }
+}
