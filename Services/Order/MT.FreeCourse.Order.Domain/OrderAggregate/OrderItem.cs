5f2bb87e7cfb1e76705ab76508ccd8c57a3439e7
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-30 22:39:36 +0300

    *[Order Service Domain Katmanı.]

---
+++ Services/Order/MT.FreeCourse.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿
+
+using MT.FreeCourse.Order.Domain.Core;
+
+namespace MT.FreeCourse.Order.Domain.OrderAggregate
+{
+    public class OrderItem :Entity
+    {
+        public string CourseId{ get; private set; }
+        public string CourseName{ get; private set; }
+        public string PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+
+        public OrderItem(string courseId, string courseName, string pictureUrl, decimal price)
+        {
+            CourseId = courseId;
+            CourseName = courseName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+
+        public void UpdateOrderItem(string courseId,string courseName,string pictureUrl, decimal price)
+        {
+            CourseId = courseId;
+            CourseName = courseName;
+            PictureUrl = pictureUrl;
+            Price = price;
+
+
+        }
+    }
+}
