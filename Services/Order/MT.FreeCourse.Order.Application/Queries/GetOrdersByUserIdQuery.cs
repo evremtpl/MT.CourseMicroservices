040d330f643c9ee77609f8234acf4bbf6203e1b1
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-31 08:22:28 +0300

    *[OrderService CQRS Pattern MediatR library eklendi.] *[Query ve Handler classlar]

---
+++ Services/Order/MT.FreeCourse.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MT.FreeCourse.Order.Application.Dtos;
+using MT.FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.FreeCourse.Order.Application.Queries
+{
+   public class GetOrdersByUserIdQuery :IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
