59e3e4db7fcfac13b460f1f4c3c57d385817df19
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-31 21:09:29 +0300

    *[Servisler API Gateway üzerinden koruma altına alınmıştır.]

--- Gateways/MT.FreeCourse.Gateway/Startup.cs
+++ Gateways/MT.FreeCourse.Gateway/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
@@ -14,10 +15,24 @@ namespace MT.FreeCourse.Gateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationSchema", opt =>
+            {
+
+                opt.Authority = _configuration["IdentityServerURL"];
+                opt.Audience = "resource_gateway";
+                opt.RequireHttpsMetadata = false;
+
+
+            });
 
             services.AddOcelot();
         }
