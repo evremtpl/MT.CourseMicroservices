1d27d6a48d1b98142cd41d652863296f033a194a
Merve TOPAL <merve.tpl@windowslve.com>
2022-03-31 07:56:18 +0300

    * [OrderService Application Katmanı Dto ve AutoMapper]

---
+++ Services/Order/MT.FreeCourse.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MT.FreeCourse.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string CourseId { get;  set; }
+        public string CourseName { get;  set; }
+        public string PictureUrl { get;  set; }
+        public decimal Price { get;  set; }
+    }
+}
